36734db804136795a8fe01a2adc9b679930a797e
borispipo <borispipo@gmail.com>
2022-10-20 07:15:15 +0100

    Fix cannont found module ./expo-ui

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import config from "./src/config";
 import { checkSavedMeters } from "$screens/Devices/utils";
 import SocketProvider from "$socket/Provider";
-require((process.env && process.env.HAS_LOCAL_EXPO_UI)?"./expo-ui":"@fto-consult/expo-ui").default({config,App:({children,APP})=>{
+require(process.env && process.env.HAS_LOCAL_EXPO_UI && "./expo-ui"||"@fto-consult/expo-ui").default({config,App:({children,APP})=>{
     const onLoginUser = ()=>{
         checkSavedMeters();
     }
