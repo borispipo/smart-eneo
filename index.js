111d7447a176564a3530db2d1ce6feec2d0eb442
borispipo <borispipo@gmail.com>
2022-10-20 07:00:54 +0100

    Fix epath

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ import config from "./src/config";
 import { checkSavedMeters } from "$screens/Devices/utils";
 import SocketProvider from "$socket/Provider";
 const ePath = require("./expo-ui-build-path");
-require(ePath.includes("@fto-consult")?"@fto-consult/expo-ui":"./expo-ui").default({config,App:({children,APP})=>{
-    const onLoginUser = ()=>{
-        checkSavedMeters();
-    }
-    APP.on(APP.EVENTS.AUTH_LOGIN_USER,onLoginUser);
-    return <SocketProvider>{children}</SocketProvider>;
-}});
+console.log(ePath," is epathttt heeeee maa")
