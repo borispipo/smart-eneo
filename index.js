53ea4a6b274e3b21fafc2fc264a78143d8a3c697
borispipo <borispipo@gmail.com>
2022-10-20 06:51:34 +0100

    Fix bug dans .index test de routine @fto/consult au lieu de @fto-consult

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import config from "./src/config";
 import { checkSavedMeters } from "$screens/Devices/utils";
 import SocketProvider from "$socket/Provider";
 const ePath = require("./expo-ui-build-path");
-require(ePath.includes("@fto/consult")?"@fto-consult/expo-ui":"./expo-ui").default({config,App:({children,APP})=>{
+require(ePath.includes("@fto-consult/")?"@fto-consult/expo-ui":"./expo-ui").default({config,App:({children,APP})=>{
     const onLoginUser = ()=>{
         checkSavedMeters();
     }
