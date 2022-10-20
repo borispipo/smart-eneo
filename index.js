5ec16444a72af875692b13962e6a89db14e3d7ce
borispipo <borispipo@gmail.com>
2022-10-20 07:08:47 +0100

    Update du package expo-ui-build-path

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import config from "./src/config";
 import { checkSavedMeters } from "$screens/Devices/utils";
 import SocketProvider from "$socket/Provider";
-const ePath = require("./expo-ui-build-path");
-console.log(ePath," is epathttt heeeee maa")
+require(process.env && process.env.HAS_LOCAL_EXPO_UI?"./expo-ui":"@fto-consult/expo-ui").default({config,App:({children,APP})=>{
+    const onLoginUser = ()=>{
+        checkSavedMeters();
+    }
+    APP.on(APP.EVENTS.AUTH_LOGIN_USER,onLoginUser);
+    return <SocketProvider>{children}</SocketProvider>;
+}});
