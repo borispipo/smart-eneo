5056f6b02789a8e582ee3d17435480627081d75b
borispipo <borispipo@gmail.com>
2022-10-20 07:20:53 +0100

    Fix gug cannot resolve ./expo-ui

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import config from "./src/config";
 import { checkSavedMeters } from "$screens/Devices/utils";
 import SocketProvider from "$socket/Provider";
-require(process.env && process.env.HAS_LOCAL_EXPO_UI && "./expo-ui"||"@fto-consult/expo-ui").default({config,App:({children,APP})=>{
+const hasLocalExpoUI = process.env && process.env.HAS_LOCAL_EXPO_UI;
+require((hasLocalExpoUI? "./expo-ui":"@fto-consult/expo-ui")).default({config,App:({children,APP})=>{
     const onLoginUser = ()=>{
         checkSavedMeters();
     }
