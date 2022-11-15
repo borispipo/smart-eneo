ee0d405a383be275e06e8194a732c6b1e8db8d2b
borispipo <borispipo@gmail.com>
2022-11-15 22:44:33 +0100

    Update du package @fto-consult/expo-ui pour prise en compte des préférences utilisateur

--- src/auth/signIn2SignOut.js
+++ src/auth/signIn2SignOut.js
@@ -1,10 +1,21 @@
 // Copyright 2022 @fto-consult/Boris Fouomene. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+import {put} from "$capi";
+import "$utils";
 
 export default {
-    upsertUser : (args)=>{
-        console.log(args," will upsert user heeee");
-        return Promise.resolve();
+    upsertUser : ({user})=>{
+        console.log(user," is upserted heeeee");
+        return put('auth/user/{0}'.sprintf(user.id),{
+            body : {
+                preferences : {
+                    avatar : undefined,
+                    theme  : user.theme,
+                }
+            }
+        }).then((data)=>{
+            console.log(data," is update preferences heeeee");
+        })
     }
 }
